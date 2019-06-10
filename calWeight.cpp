d7fbf77a3c4ee248449a01d0d28439a3ff354d86
wkd3475 <wkd3475@gmail.com>
2019-06-10 16:08:06 +0900

    update

--- calWeight.cpp
+++ calWeight.cpp
@@ -10,18 +10,20 @@ using namespace std;
 
 static double get_file_score(const int begin, const int end, const time_t accessTime[NUMBEROFSAVE], const time_t reference, vector<time_t> const &diff);
 static double get_subdir_score(const int begin, const int end, const time_t accessTime[NUMBEROFSAVE], const time_t reference, vector<time_t> const &diff);
+static double get_size_score(const off_t size);
 
 double calWeight(string path, time_t reference, vector<time_t> const &diff) {
     struct stat stat;
     struct s2hData s2hdata;
-	double file_score, subdir_score, total_score;
+	double file_score, subdir_score, size_score, total_score;
     getInfo(path, &stat, &s2hdata);
 
-	file_score = get_file_score(s2hdata.begin, s2hdata.end, s2hdata.accessTime, reference, diff);
-	subdir_score = get_subdir_score(s2hdata.beginSubdir, s2hdata.endSubdir, s2hdata.accessTimeSubdir, reference, diff);
+	file_score = get_file_score(s2hdata.begin, s2hdata.end, s2hdata.accessTime, reference, diff);//0~num_of_diff
+	subdir_score = get_subdir_score(s2hdata.beginSubdir, s2hdata.endSubdir, s2hdata.accessTimeSubdir, reference, diff);//0~num_of_diff
+	size_score = get_size_score(s2hdata.sizeOfDir);//0~16
 
 	total_score = file_score + subdir_score;//weight???
-
+	
 	return total_score;
 }
 
@@ -33,17 +35,26 @@ double get_file_score(const int begin, const int end, const time_t accessTime[NU
 	else
 		length = NUMBEROFSAVE;
 	
+	int diff_len = 0;
 	for(vector<time_t>::const_iterator iter = diff.begin(); iter!=diff.end(); iter++) {
 		for(int i=0; i<length; i++) {
 			if(*iter < reference - accessTime[i]) {
 				score++;
 			}
 		}
+		diff_len++;
 	}
 
 	score = score / length;
 
-	return 16 * NUMBEROFSAVE - score;
+	printf("begin : %d\n", begin);
+	printf("end : %d\n", end);
+	printf("length : %d\n", score);
+	printf("diff_len : %d\n", diff_len);
+	printf("score : %lf\n", score);
+	printf("==================\n");
+
+	return diff_len - score;
 }
 
 static double get_subdir_score(const int begin, const int end, const time_t accessTime[NUMBEROFSAVE], const time_t reference, vector<time_t> const &diff) {
@@ -54,15 +65,21 @@ static double get_subdir_score(const int begin, const int end, const time_t acce
 	else
 		length = NUMBEROFSAVE;
 
+	int diff_len = 0;
 	for(vector<time_t>::const_iterator iter = diff.begin(); iter!=diff.end(); iter++) {
 		for(int i=0; i<length; i++) {
 			if(*iter < reference - accessTime[i]) {
 				score++;
 			}
 		}
+		diff_len++;
 	}
 
 	score = score / length;
 
-	return 16 * NUMBEROFSAVE - score;
+	return diff_len - score;
+}
+
+static double get_size_score(const off_t size) {
+	return 0;
 }
