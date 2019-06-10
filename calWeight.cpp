8b1b533178d8d37d62bbd60a21983529ed9bcbf2
wkd3475 <wkd3475@gmail.com>
2019-06-10 18:05:19 +0900

    update

--- calWeight.cpp
+++ calWeight.cpp
@@ -1,7 +1,7 @@
 #include<vector>
 #include<utility>
 #include<sys/stat.h>
-#include<string> 
+#include<string>
 #include<math.h>
 #include"ssdToHdd.h"
 
@@ -17,11 +17,13 @@ double calWeight(string path, time_t reference, vector<time_t> const &diff) {
     struct s2hData s2hdata;
 	double file_score, subdir_score, size_score, total_score;
     getInfo(path, &stat, &s2hdata);
+	printf("%s\n", path.c_str());
 
 	file_score = get_file_score(s2hdata.begin, s2hdata.end, s2hdata.accessTime, reference, diff);//0~num_of_diff
 	subdir_score = get_subdir_score(s2hdata.beginSubdir, s2hdata.endSubdir, s2hdata.accessTimeSubdir, reference, diff);//0~num_of_diff
 	size_score = get_size_score(s2hdata.sizeOfDir);//0~16
-
+	//100MB 1GB 5GB
+	//1.1 1.2 1.3
 	total_score = file_score + subdir_score;//weight???
 	
 	return total_score;
@@ -35,26 +37,20 @@ double get_file_score(const int begin, const int end, const time_t accessTime[NU
 	else
 		length = NUMBEROFSAVE;
 	
-	int diff_len = 0;
 	for(vector<time_t>::const_iterator iter = diff.begin(); iter!=diff.end(); iter++) {
 		for(int i=0; i<length; i++) {
 			if(*iter < reference - accessTime[i]) {
 				score++;
 			}
 		}
-		diff_len++;
 	}
+
+	if(length==0)
+		return 0.0;
 
 	score = score / length;
 
-	printf("begin : %d\n", begin);
-	printf("end : %d\n", end);
-	printf("length : %d\n", score);
-	printf("diff_len : %d\n", diff_len);
-	printf("score : %lf\n", score);
-	printf("==================\n");
-
-	return diff_len - score;
+	return score;
 }
 
 static double get_subdir_score(const int begin, const int end, const time_t accessTime[NUMBEROFSAVE], const time_t reference, vector<time_t> const &diff) {
@@ -65,21 +61,23 @@ static double get_subdir_score(const int begin, const int end, const time_t acce
 	else
 		length = NUMBEROFSAVE;
 
-	int diff_len = 0;
 	for(vector<time_t>::const_iterator iter = diff.begin(); iter!=diff.end(); iter++) {
 		for(int i=0; i<length; i++) {
 			if(*iter < reference - accessTime[i]) {
 				score++;
 			}
 		}
-		diff_len++;
 	}
+
+	if(length==0)
+		return 0.0;
 
 	score = score / length;
 
-	return diff_len - score;
+	return score;
 }
 
 static double get_size_score(const off_t size) {
+	printf("size : %ld\n", size);
 	return 0;
 }
